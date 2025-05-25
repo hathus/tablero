8574fde55ba298f4aa36db3ad55402f6435f5cf0
Hathus <trilogyti@gmail.com>
2025-05-24 22:06:51 -0600

    database tables
    basic tables for the system

---
+++ database/migrations/2025_05_25_021316_create_ficha_compromisos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ficha_compromisos', function (Blueprint $table) {
+            $table->id();
+            $table->string('oid');
+            $table->integer('numero_compromiso');
+            $table->string('nombre_compromiso');
+            $table->string('nombre_dependencia');
+            $table->string('siglas_dependencia');
+            $table->string('inter_dependencias');
+            $table->integer('presupuesto_total');
+            $table->integer('presupuesto_adicional');
+            $table->integer('anio_aplicado');
+            $table->integer('presupuesto_aplicado');
+            $table->string('status_compromiso');
+            $table->foreignId('compromiso_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ficha_compromisos');
+    }
+};
